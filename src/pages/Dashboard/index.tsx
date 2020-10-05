a14c860f7b64c07eefe03b1fd2c82ebae1542f3d
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-10-05 00:02:07 -0300

    Header do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React, { useCallback } from 'react';
 
-import { Container } from './styles';
 import { useAuth } from '../../hooks/auth';
+import { useNavigation } from '@react-navigation/native';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
     <Container>
-      <Button title="Sair" onPress={signOut} />
+      <Header>
+        <HeaderTitle>
+          Bem-vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
     </Container>
   );
 };
